fec125c19cb59afd0a36860f20743ea6d2649f41
Shana026 <shana2000.sebban@gmail.com>
2024-10-28 22:36:50 +0200

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useState } from 'react';
+import breakfast from './assets/breakfast.jpg'
+import lunch from './assets/lunch.jpg'
 
-function App() {
+/***********************************************************************************
+ Component CategoryFilter - represents a category filter with its image and name
+ Gets prop category - an object representing the category, containing its name and img
+ Gets prop onCategoryClick - a function that will be call when the category is clicked
+ This function is passed down from the parent component and will handle filtering
+ recipes based on the selected category.
+ Return: a circular image and the category name and calls onCategoryClick when clicked 
+***********************************************************************************/
+function CategoryFilter({category, onCategoryClick}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="category-filter" onClick={() => onCategoryClick(category.name)}>
+      <img src={category.img} alt={category.name}/>
+      <p>{category.name}</p>
     </div>
   );
 }
 
-export default App;
+
+/***********************************************************************************
+ Component ListCategoryFilter - Contains all CategoryFilter components
+ Gets prop 
+ Return:  
+***********************************************************************************/
+function ListCategoryFilter({ categories, onCategoryClick }) {
+
+  // for each category in the list, create a CategoryFilter
+  const listCategoryFilter = categories.map(category => (
+    <CategoryFilter key={category.name} category={category} onCategoryClick={onCategoryClick} />
+  ));
+
+  return (
+    <div className="list-category-filters">
+      {listCategoryFilter}
+    </div>
+  );
+}
+
+
+/***********************************************************************************
+ Component Page
+ Gets prop 
+ Return:  
+***********************************************************************************/
+export default function Page() {
+
+  const categories = [
+    { name: 'Breakfast', img: breakfast },
+    { name: 'Lunch', img: lunch },
+  ];
+
+  function onCategoryClick(category) {
+    alert(`You clicked on ${category.name}`);
+  };
+
+  return (
+    <div className="page">
+      <ListCategoryFilter categories={categories} onCategoryClick={onCategoryClick} />
+      <h3>You click on {categories[0].name}</h3>
+    </div>
+  );
+}
