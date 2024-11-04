8c16c1314d492ce81b059b05f5dbe6284bd78e76
Shana026 <shana2000.sebban@gmail.com>
2024-11-04 15:09:24 +0200

    design recipe details page

--- src/AppData.tsx
+++ src/AppData.tsx
@@ -2,7 +2,7 @@ import babyfoursixmonths from './assets/baby4-6months.jpg'
 import babysixheightmonths from './assets/baby6-8months.jpg'
 import babyninetwelvemonths from './assets/baby9-12months.jpg'
 import babytwelvemonthsandplus from './assets/baby12+months.jpg'
-import sweet from './assets/sweet3.jpg'
+import sweet from './assets/sweet.jpg'
 import meal from './assets/meal.jpg'
 import fingerFood from './assets/finger_food.jpg'
 import pumpkinSoup from './assets/pumpkin_soup.jpg'
@@ -12,12 +12,13 @@ import potatoFries from './assets/potato_fries.jpg'
 import fruitsCompote from './assets/fruits_compote.jpg'
 import cookies from './assets/cookies.jpg'
 import chickenSoup from './assets/chicken_soup.jpg'
+import baby2 from './assets/baby2.jpg'
 
 export const categoriesData = [
   { name: '4-6 months', image: babyfoursixmonths },
-  { name: '6-8 months', image: babysixheightmonths },
+  { name: '6-8 months', image:  babysixheightmonths},
   { name: '9-12 months', image: babyninetwelvemonths },
-  { name: '12+ months', image: babytwelvemonthsandplus },
+  { name: '12+ months', image: baby2 },
   { name: 'Soup', image: soup },
   { name: 'Meal', image: meal },
   { name: 'Finger Food', image: fingerFood },
@@ -31,7 +32,8 @@ export const recipesData = [
     age: 4,
     image: fruitsCompote,
     categories: ['4-6 months', '6-8 months', '9-12 months', '12+ months', 'Sweet'],
-    description: '',
+    description: 'Cook and mix all the fruits',
+    ingredients: ['Apple', 'Pear', 'Banana'],
   },
   {
     id: '2',
@@ -39,7 +41,8 @@ export const recipesData = [
     age: 8,
     image: pancakes,
     categories: ['6-8 months', '9-12 months', '12+ months', 'Sweet'],
-    description: '',
+    description: 'Mix all the ingredients',
+    ingredients: ['4 cas of flour', '1 egg', '1 applesauce'],
   },
   {
     id: '3',
@@ -48,6 +51,7 @@ export const recipesData = [
     image: pumpkinSoup,
     categories: ['4-6 months', '6-8 months', '9-12 months', '12+ months', 'Soup'],
     description: '',
+    ingredients: ['Potato', 'Pumpkin', 'Carrot'],
   },
   {
     id: '4',
@@ -56,6 +60,7 @@ export const recipesData = [
     image: potatoFries,
     categories: ['9-12 months', '12+ months', 'Finger Food'],
     description: '',
+    ingredients: ['potato', 'oil'],
   },
   {
     id: '5',
@@ -64,6 +69,7 @@ export const recipesData = [
     image: cookies,
     categories: ['12+ months', 'Finger Food', 'Sweet'],
     description: '',
+    ingredients: ['flour', 'egg', 'sugar'],
   },
   {
     id: '6',
@@ -72,5 +78,6 @@ export const recipesData = [
     image: chickenSoup,
     categories: ['6-8 months', '9-12 months', '12+ months', 'Soup', 'Meal'],
     description: '',
+    ingredients: ['potato', 'carrot', 'chicken'],
   },
 ];
