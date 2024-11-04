230fae9eaf3edff27572bbe230f560b563755863
Shana026 <shana2000.sebban@gmail.com>
2024-11-04 22:35:49 +0200

    fix

--- src/AddRecipe.tsx
+++ src/AddRecipe.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './AddRecipeForm.css';
+import './AddRecipe.css';
 
 /***********************************************************************************
   Component AddRecipeForm: form
